3cd9621edb8c38f8c617fca81e971b946519e0ad
ViktoriiaYakaba <yakaba@bk.ru>
2024-09-30 16:13:30 +0200

    add newsReducer

---
+++ src/redux/news/operations.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = 'https://petlove.b.goit.study/api';
+
+export const fetchNews = createAsyncThunk(
+    'news/fetchAll',
+    async ({ page, searchQuery }, thunkAPI) => {
+      let response;
+      try {
+        if (!searchQuery) {
+          response = await axios.get(`/news?page=${page}`);
+        } else {
+          response = await axios.get(`/news?page=${page}&keyword=${searchQuery}`);
+        }
+        return response.data;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+)
